0d5e006dc35ec43d75c2ec1855088f0bcdc8504d
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2025-06-20 17:28:44 +0530

    Changes

--- server/index.js
+++ server/index.js
@@ -1,55 +1,41 @@
-import express from 'express';
-import cors from 'cors'
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import {
-  ApolloServerPluginLandingPageLocalDefault
-} from '@apollo/server/plugin/landingPage/default';
-import axios from 'axios';
-import bodyParser from 'body-parser';
-
-const todos = [
-  { id: '1', title: 'Learn GraphQL', completed: false },
-  { id: '2', title: 'Build Apollo Server', completed: true }
-];
-
-const typeDefs = `
-  type Todo {
-    id: ID!
-    title: String!
-    completed: Boolean
-  }
-  type Query {
-    getTodos: [Todo]
-  }
-`;
-
-// const resolvers = {
-//   Query: {
-//     getTodos: () => todos  // <-- return the array here
-//   }
-// };
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 async function startServer() {
 
   const app = express();
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers:{
-        Query: {
-            getTodos:async ()=>(await axios.get('https://jsonplaceholder.typicode.com/todos')).data
-        }
-    },
- });
+  app.use(express.json());
 
+  const server = new ApolloServer({
+    typeDefs:`
+          type ToDO{ 
+            id : ID!
+            title : String!
+            completed : Boolean!
+          }
+
+          type Query {
+              getToDo : [ToDO]
+          }
+        `,
+      resolvers:{
+        
+      }
+  });
+
+  app.use(bodyParser.json());
+  app.use(cors());
   await server.start();
 
-  app.use('/graphql', cors(), bodyParser.json(), expressMiddleware(server)
-  );
-
-  app.listen(8000, () =>
-    console.log('Server ready at http://localhost:8000/graphql')
-  );
+  app.use("/graphql", expressMiddleware(server));
+  app.use(express.json());
+  //   const port = process.env.PORT || 3000;
+  app.listen(8000, () => {
+    console.log("server is running ");
+  });
 }
 
-startServer(); 
+startServer();
